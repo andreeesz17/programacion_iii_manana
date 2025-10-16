9a2faeccbc7c466458e65f33cd3388d0f2116f42
andreeesz17 <andryzambrano1709@gmail.com>
2025-10-16 09:59:16 -0500

    Clase 2025-10-16

---
+++ modulo2/javascript/02_variables.js
@@ -0,0 +1,35 @@
+console.log("Variables en Javascript");
+
+saludo="Hola utilizando variables en Javascript";
+console.log("Variable saludo",saludo);
+console.log("Tipo de variable",typeof(saludo));
+
+numero=123;
+console.log("Variable numero",numero);
+console.log("Tipo de variable",typeof(numero));
+
+booleaneo=true;
+console.log("Variable booleaneo",booleaneo);
+console.log("Tipo de variable",typeof(booleaneo));
+
+indefinida=undefined;
+console.log("Variable indefinida",indefinida);
+console.log("Tipo de variable",typeof(indefinida));
+
+nula=null;
+console.log("Variable nula",nula);
+console.log("Tipo de variable",typeof(nula));
+
+bigentero=123456789123456789123456789;
+console.log("Variable BiInt",bigentero);
+console.log("Tipo de variable",typeof(bigentero));
+
+persona={nombre:"Andrés",edad:19};
+console.log("Variable Objeto",persona);
+console.log("Tipo de variable",typeof(persona));
+
+numeros=[1,2,3,4,5];
+console.log("Variable Array",numeros);
+console.log("Tipo de variable",typeof(numeros));
+
+
---
+++ modulo2/javascript/03_operadores_aritmeticos.js
@@ -0,0 +1,57 @@
+console.log("Operadores Aritmeticos en Javascript");
+
+numero1=10;
+numero2=20;
+
+console.log("suma");
+suma=numero1+numero2;
+console.log("Resultado de la suma",suma);
+
+console.log("resta");
+resta=numero1-numero2;
+console.log("Resultado de la resta",resta);
+
+console.log("multiplicacion");
+multiplicacion=numero1*numero2;
+console.log("Resultado de la multiplicacion",multiplicacion);
+
+console.log("division");
+division=numero1/numero2;
+console.log("Resultado de la division",division);
+
+console.log("modulo");
+modulo=numero1%numero2;
+console.log("Resultado del modulo",modulo);
+
+console.log("potencia");
+exponente=numero1**numero2;
+console.log("Resultado del exponente",potencia);
+
+console.log("incremento");
+numero1++;
+console.log("Resultado del incremento",numero1);
+
+console.log("decremento");
+numero1--;
+console.log("Resultado del decremento",numero1);
+
+console.log("comparacion");
+console.log("igualdad debil ==", "5"==5);
+console.log("igualdad estricta ===","5"===5);
+console.log("diferente debil !=", "5"!=5);
+console.log("diferente estricto !==", "5"!==5);
+console.log("mayor que >", "5">3);
+console.log("menor que <", "5"<3);
+console.log("mayor o igual que >=", "5">=3);
+console.log("menor o igual que <=", "5"<=3);
+
+console.log("operadores logicos");
+console.log("y And &&", "5"<=5 && "5">=5);
+console.log("o Or ||", "5"<=5 || "6"<4);
+console.log("negacion Not !", !(5==5));
+
+
+
+
+
+
