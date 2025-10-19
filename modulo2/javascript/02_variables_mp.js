dd13f44f1b2826b2fde1daed4d1a94a8d3fe48d9
andreeesz17 <andryzambrano1709@gmail.com>
2025-10-19 18:19:50 -0500

    Tarea Segumiento repositorio Semana 3

---
+++ modulo2/javascript/02_variables_mp.js
@@ -0,0 +1,39 @@
+console.log("Variables en SafeCase Police");
+
+nombreSistema = "SafeCase Police - Gestión de Expedientes";
+console.log("Nombre del Sistema:", nombreSistema);
+console.log("Tipo de variable:", typeof(nombreSistema));
+
+numeroExpediente = 20251014;
+console.log("Número de Expediente:", numeroExpediente);
+console.log("Tipo de variable:", typeof(numeroExpediente));
+
+expedienteActivo = true;
+console.log("¿Expediente Activo?:", expedienteActivo);
+console.log("Tipo de variable:", typeof(expedienteActivo));
+
+fechaCierre = undefined;
+console.log("Fecha de Cierre:", fechaCierre);
+console.log("Tipo de variable:", typeof(fechaCierre));
+
+oficialAsignado = null;
+console.log("Oficial Asignado:", oficialAsignado);
+console.log("Tipo de variable:", typeof(oficialAsignado));
+
+idRegistro = 202510141234567890123456789n;
+console.log("ID de Registro:", idRegistro);
+console.log("Tipo de variable:", typeof(idRegistro));
+
+expediente = {
+    codigo: "EXP-2025-001",
+    tipo: "Investigación",
+    estado: "Abierto"
+};
+console.log("Datos del Expediente:", expediente);
+console.log("Tipo de variable:", typeof(expediente));
+
+involucrados = ["Testigo 1", "Testigo 2", "Sospechoso 1"];
+console.log("Lista de Involucrados:", involucrados);
+console.log("Tipo de variable:", typeof(involucrados));
+
+
