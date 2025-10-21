88f3a4643a93babd4655b3b4b69e16f644f4fe94
andreeesz17 <andryzambrano1709@gmail.com>
2025-10-21 10:03:49 -0500

    clase 2025-10-21

---
+++ modulo2/javascript/05_bucles.js
@@ -0,0 +1,23 @@
+for (let i=1; i<=10; i++) {
+    console.log("Número:", i);
+}
+suma=0;
+for (let i=1; i<=10; i++) {
+    suma += i;
+}
+console.log("Suma total:", suma);
+
+let numero = 4;
+console.log("Tabla de multiplicar del", numero);
+for (let i=1; i<=10; i++) {
+    console.log(numero, "x", i, "=", numero*i);
+}
+
+nombre="francisco";
+for (let i=0; i<=nombre.length; i++) {
+    console.log(nombre[i]);
+}
+nombree="francisco";
+for (let i=0; i<nombree.length; i++) {
+    console.log(nombree[i]);
+}
