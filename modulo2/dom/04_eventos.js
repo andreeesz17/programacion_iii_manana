c8eee56309e01d56c8abc632c2e05dcf42f2c50c
andreeesz17 <andryzambrano1709@gmail.com>
2025-10-28 10:01:10 -0500

    poo

--- modulo2/dom/04_eventos.js
+++ modulo2/dom/04_eventos.js
@@ -1,9 +1,21 @@
-let parrafo= null;
-document.getElementById('crear').addEventListener('click',()=>{
+let parrafo = null;
+document.getElementById('crear').addEventListener('click', () => {
     parrafo = document.createElement('p');
-    parrafo.textContent="Párrafo dinámico desde Javascript";
+    parrafo.textContent = "Párrafo dinámico desde Javascript";
     document.getElementById('contenedor').appendChild(parrafo);
 });
-document.getElementById('eliminar').addEventListener('click',()=>{
+document.getElementById('eliminar').addEventListener('click', () => {
     if (parrafo) parrafo.remove();
+})
+
+function saludar() {
+    alert("Hola desde la función");
+}
+
+document.getElementById('parrafo').addEventListener('dbclick', () => {
+    alert("Hiciste doble click en el párrafo");
+})
+
+document.getElementById('nombre').addEventListener('input', (e) => {
+    console.log("El valor del input es: ", e.target.value);
 })
